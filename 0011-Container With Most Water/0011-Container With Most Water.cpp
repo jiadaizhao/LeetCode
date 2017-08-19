960e9633141c6f50d5dcfb23ede68756ff462098
jiadaizhao <jiadaizhao@gmail.com>
2017-08-18 19:33:02 -0700

    Update 0011-Container With Most Water.cpp

--- 0011-Container With Most Water/0011-Container With Most Water.cpp
+++ 0011-Container With Most Water/0011-Container With Most Water.cpp
@@ -5,7 +5,7 @@ public:
         int start = 0, end = height.size() - 1;
         while (start < end)
         {
-            maxA = max(maxA, min(height[start], height[end]) * (end -start));
+            maxA = max(maxA, min(height[start], height[end]) * (end - start));
             if (height[start] <= height[end])
             {
                 ++start;
