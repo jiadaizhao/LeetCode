f0006b3ebd9d1c3f8540f461ae248af5916d8028
jiadaizhao <jiadaizhao@gmail.com>
2017-10-16 11:29:23 -0700

    Update 0066-Plus One.cpp

--- 0066-Plus One/0066-Plus One.cpp
+++ 0066-Plus One/0066-Plus One.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
         int i = n - 1;
-        while (i < n && digits[i] == 9)
+        while (i >= 0 && digits[i] == 9)
         {
             digits[i] = 0;
             --i;
