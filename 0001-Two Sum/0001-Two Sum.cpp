12d7ae6c2d80250273f6f193aa402205b1b0cf1c
M Squared <birthofgenesis@yahoo.com>
2017-12-05 10:57:40 +0000

    Update 0001-Two Sum.cpp

--- 0001-Two Sum/0001-Two Sum.cpp
+++ 0001-Two Sum/0001-Two Sum.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> table;
-        vector<int> result(2);
-        for (int i = 0; i < nums.size(); ++i) {
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> table;
+        std::vector<int> result(2);
+        for (auto i = 0; i < nums.size(); ++i) {
             if (table.find(target - nums[i]) != table.end()) {
                 result[0] = table[target - nums[i]];
                 result[1] = i;
